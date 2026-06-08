8cf40892edc4c6372dd6c0a528b420eddf56d6e0
Fatma <fatma.abdullayewa@gmail.com>
2026-06-08 18:17:49 +0300

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,49 @@
+import { Image, StyleSheet, Text } from "react-native";
+import Logo from "../assets/shelfie.webp";
+import { Link } from "expo-router";
+import ThemedView from "../components/ThemedView";
+import Spacer from "../components/Spacer";
+import ThemedText from "../components/ThemedText";
+
+const Home = () => {
+  return (
+    <ThemedView style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+      <Spacer height={20} />
+      <ThemedText style={styles.title} title={true}>
+        The Number 1
+      </ThemedText>
+      <Spacer height={10} />
+      <ThemedText>Reading List App</ThemedText>
+      <Spacer />
+      <Link href="/login" style={styles.link}>
+        <ThemedText>Login Page</ThemedText>
+      </Link>
+      <Link href="/register" style={styles.link}>
+        <ThemedText>Register Page</ThemedText>{" "}
+      </Link>
+    </ThemedView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  img: {
+    width: 100,
+    height: 70,
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
+  },
+});
