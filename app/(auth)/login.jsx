96cf74d2c5592c85347a40a0f71ec95be6200465
Fatma <fatma.abdullayewa@gmail.com>
2026-06-09 00:52:30 +0300

    pressable themedbutton component

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,16 +1,25 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet, Text } from "react-native";
 import ThemedView from "../../components/ThemedView";
 import Spacer from "../../components/Spacer";
 import ThemedText from "../../components/ThemedText";
 import { Link } from "expo-router";
+import ThemedButton from "../../components/ThemedButton";
 
 const Login = () => {
+
+  const handleSubmit =()=>{
+    console.log("login form submitted");
+  }
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title={true} style={styles.title}>
         Login to Your Account
       </ThemedText>
+    <ThemedButton onPress={handleSubmit}>
+      <Text style={{color: "#f2f2f2"}}>Login</Text>
+    </ThemedButton>
       <Spacer height={100} />
       <Link href="/register">
         <ThemedText style={{ textAlign: "center" }}>
@@ -26,11 +35,13 @@ export default Login;
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "center",
+    alignItems: "center"
   },
   title:{
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30
-  }
+  },
+
 });
