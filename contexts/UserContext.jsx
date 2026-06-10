531dbcfcae9c9c69733e5ae4aa9c8454da3548ae
Fatma <fatma.abdullayewa@gmail.com>
2026-06-10 03:51:09 +0300

    implement user logout

--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -1,42 +1,46 @@
-import { createContext, useState } from "react"
-import { account } from "../lib/appwrite"
-import { ID } from "react-native-appwrite"
+import { createContext, useState } from "react";
+import { account } from "../lib/appwrite";
+import { ID } from "react-native-appwrite";
 
-export const UserContext = createContext()
+export const UserContext = createContext();
 
 export function UserProvider({ children }) {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   async function login(email, password) {
     try {
-      await account.createEmailPasswordSession(email, password)
-      const response = await account.get()
-      setUser(response)
-
+      await account.createEmailPasswordSession(email, password);
+      const response = await account.get();
+      setUser(response);
     } catch (error) {
-     throw Error(error.message)
+      throw Error(error.message);
     }
   }
 
   async function register(email, password) {
     try {
-      await account.create(ID.unique(), email, password)
-      await login(email, password)
+      await account.create(ID.unique(), email, password);
+      await login(email, password);
     } catch (error) {
-     throw Error(error.message)
+      throw Error(error.message);
     }
   }
 
   async function logout() {
-
+    await account.deleteSession("current");
+    setUser(null);
   }
 
   return (
-    <UserContext.Provider value={{ 
-      user, login, logout, register,
-    }}>
+    <UserContext.Provider
+      value={{
+        user,
+        login,
+        logout,
+        register,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
 }
-
